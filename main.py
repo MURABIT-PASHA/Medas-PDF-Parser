951689d756160131ed4dac40f46e04a2f6e7962c
MURABIT-PASHA <akdoganmurabit@gmail.com>
2023-07-16 01:03:16 +0300

    V1.0.0

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
+import os
+import threading
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivymd.app import MDApp
 from PyPDF2 import PdfWriter, PdfReader
-
-inputpdf = PdfReader(open("2022.pdf", "rb"))  # pdf dosya ismi yazılacak
-
-for i in range(len(inputpdf.pages)):
-    output = PdfWriter()
-    output.add_page(inputpdf.pages[i])
-    # dosyanın çıkarılacağı klasör ismi yazılacak
-    with open("C:/Users/mrakd/Desktop/tk/2022/%s.pdf" % i, "wb") as outputStream:
-        output.write(outputStream)
+from kivymd.toast import toast
 
 
-from PyPDF2 import PdfReader
+class DragDropPdfApp(MDApp):
+    progress_value = 0
 
-counter = 0
-for k in range(4737):
-    # range verirken kaç tane dosya varsa sayı olarak yazılacak.
-    # altta dosya dizinleri kısmı da düzenlenecek.
-    pdfname = "C:/Users/mrakd/Desktop/tk/2022/" + str(k) + ".pdf"
-    reader = PdfReader(pdfname)
-    gg = reader.pages[0]
-    parpage = gg.extract_text()
-    splitt = parpage.split("no'lu tesisata")
-    tesisat_no = splitt[0].split(",")[-1]
-    pparpage = gg.extract_text()
-    ssparpge = pparpage.split("no'lu sayac")
-    sayac_no = ssparpge[0].split("Tarihinde")[-1]
-    print(str(k) + str(tesisat_no) + " sayac: " + str(sayac_no))
-    newpdfname = str(k) + "-" + str(tesisat_no) + "-" + str(sayac_no)
-    newpdfnamey = newpdfname.replace("\n", "").replace(" ", "")
-    output = PdfWriter()
-    output.add_page(gg)
-    with open("C:/Users/mrakd/Desktop/tk/2022x/%s.pdf" % newpdfnamey, "wb") as outputStream:
-        output.write(outputStream)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source_file_name = "Null"
+        self.desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'MedasPDF')
+        self.screen = Builder.load_file('./main.kv')
+
+    def build(self):
+        self.title = 'MEDAŞ PDF Parser'
+        self.icon = './icon.ico'
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Dark"
+        Window.size = (640, 480)
+        Window.bind(on_dropfile=self.on_file_drop)
+        return self.screen
+
+    def on_file_drop(self, window, file_path):
+        self.source_file_name = file_path.decode("utf-8")
+
+    def start_long_running_task(self):
+        if self.source_file_name != "Null":
+            toast("İşleminiz başlatılıyor", duration=1.0)
+            thread = threading.Thread(target=self.start_parse)
+            thread.start()
+            toast("İşleminiz tamamlandı")
+        else:
+            toast("Bir dosya seçmediniz")
+
+    def create_desktop_path(self) -> bool:
+        try:
+            os.makedirs(self.desktop_path)
+            return True
+        except FileExistsError:
+            return True
+
+    def start_parse(self):
+        input_pdf = PdfReader(open(self.source_file_name, "rb"))
+        for i in range(len(input_pdf.pages)):
+            output = PdfWriter()
+            output.add_page(input_pdf.pages[i])
+            with open("./temporary/%s.pdf" % i, "wb") as outputStream:
+                output.write(outputStream)
+
+        for k in range(len(input_pdf.pages)):
+            pdf_name = "./temporary/" + str(k) + ".pdf"
+            reader = PdfReader(pdf_name)
+            read_page = reader.pages[0]
+            first_extracted_part = read_page.extract_text()
+            first_split = first_extracted_part.split("no'lu tesisata")
+            installation_number = first_split[0].split(",")[-1]
+            second_split = first_extracted_part.split("no'lu sayac")
+            counter_number = second_split[0].split("Tarihinde")[-1]
+            new_pdf_name = (str(k) + "-" + str(installation_number) + "-" + str(counter_number)) \
+                .replace("\n", "").replace(" ", "")
+            output = PdfWriter()
+            output.add_page(read_page)
+            if self.create_desktop_path():
+                with open(f"{self.desktop_path}/{new_pdf_name}.pdf", "wb") as outputStream:
+                    output.write(outputStream)
+                    self.root.ids.progress_bar.value = 100 * (k + 1) / len(input_pdf.pages)
+            else:
+                toast('Dosya sistemi hatası var')
+        for file_name in os.listdir('./temporary'):
+            file_path = os.path.join('./temporary', file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+
+if __name__ == '__main__':
+    DragDropPdfApp().run()
